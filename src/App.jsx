40e17ede81770151ae648f9c07805adec935c753
haidang <tieuhaidang1@gmail.com>
2026-04-04 21:47:17 +0700

    Header va Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import UsersPage from './UsersPage'
+import Footer from "./components/layout/Footer.jsx";
+import Header from "./components/layout/Header.jsx";
 
 function App() {
-  return
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
