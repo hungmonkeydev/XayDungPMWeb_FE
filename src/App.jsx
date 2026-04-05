58ee99e82c4bf3ebb02d02d1194d42a1f04a8fba
haidang <tieuhaidang1@gmail.com>
2026-04-05 22:10:38 +0700

    giao diện Dashboard Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 import Footer from "./components/layout/Footer.jsx";
 import Header from "./components/layout/Header.jsx";
+import AdminLayout from "./pages/admin/AdminLayout.jsx";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      {/* <Header />
+      <Footer /> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
